2cf204d46f4e02c3f9a497134445aa84b9c4b634
Hwajin <164116321+2hwajin@users.noreply.github.com>
2024-10-29 13:03:35 +0900

    Update lspro.sh

--- lspro.sh
+++ lspro.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-items=()
+print_contents() {
+    local dir=$1
+    local indent=$2
+    # local dir = $1 : 현재 디렉토리 경로 저장
+    # local indent = $2 : 출력할 때 들여쓰기 조정하기 위한 문자열 저장
+    
+    for sub_item in "$dir"/*; do
+        if [ -e "$sub_item" ]; then
+            #basename : 파일명 호출
+            filename=$(basename "$sub_item")
+            echo "${indent}|$filename"
 
-for item in $(ls); do
-    items+=("$item")
-done
+            # 항목이 디렉토리일 경우 재귀 호출
+            if [ -d "$sub_item" ]; then
+                print_contents "$sub_item" "${indent}|    "
+            fi
+        else
+            echo "${indent}|    |"
+        fi
+    done
+}
+
+items=(*)
 
 for item in "${items[@]}"; do
     if [ -d "$item" ]; then
         echo "|$item"
-        for sub_item in "$item"/*; do
-            if [ -e "$sub_item" ]; then
-                filename=$(echo "$sub_item" | cut -d '/' -f 2-)
-                echo "|    |$filename"
-            else
-                echo "|    |    |"
-            fi
-                if [ -d "$sub_item" ]; then
-                    for low_item in "$sub_item"/*; do
-                        if [ -e "$low_item" ]; then
-                            filename=$(echo "$low_item" | cut -d '/' -f 3-)
-                            echo "|    |    |$filename"
-                        else
-                            echo "|    |    |    |"
-                        fi
-                    done
-                fi
-            done
+        print_contents "$item" "|    "
     else
         echo "|$item"
     fi
@@ -35,5 +36,4 @@ done
 exit 0
 
 # bash lspro.sh로 실행
-# 단점 : 아직 하위 파일 전부가 아닌 디렉토리 => 디렉토리 (2단계)까지만 접근 가능
-# 진행 중 : 코드에서 반복 가능한 부분을 찾아 반복문으로 만들기
+# 완료 : 코드에서 반복 가능한 부분을 찾아 반복문으로 만들기 => 재귀함수로 구현
